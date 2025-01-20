5547378cc79f50b4f587bb8fba3402761f7f4da3
Ram Pawar <125140500+Rampawardz@users.noreply.github.com>
2025-01-20 17:46:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(express.static(path.join(__dirname, "public"))); // Serve static fronten
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
